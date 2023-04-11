370210d5169f280c71a56dd0c1e86e68ccfa9bb9
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-04-11 22:45:40 +0300

    props to post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import p from './Post.module.css';
 
-const Post = () => {
+const Post = props => {
 	return (
 		<div className={p.post}>
-			<img src="https://i.pravatar.cc/300" alt="" />
-			<p>Post1</p>
+			<img className={p.post__image} src={props.imgId} alt="" />
+			<p className={p.post__description}>{props.message}</p>
 		</div>
 	);
 };
