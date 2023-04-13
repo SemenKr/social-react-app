d7ecd3637b7bc539cca547319bd350b15b293600
Семён Крекотун <samkrekotyn@gmail.com>
2023-04-13 14:34:53 +0300

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import p from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div className={p.main_content}>
             Main Content
-            <div>
-                <img src={'https://picsum.photos/800/200?random=1?grayscale'}/>
-            </div>
-            <div>ava + descriptions</div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     );
