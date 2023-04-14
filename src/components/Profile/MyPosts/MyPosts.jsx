12ba64cfa24c9da8763eb91ec21c7a8083046c63
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-04-14 10:58:42 +0300

    MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import Post from './Post/Post';
-import './MyPosts.module.css';
+import p from './MyPosts.module.css';
 
 const MyPosts = () =>
-	(
+(
+	<div className={p.postsBlock}>
 		<div>
-			<div>
-				wtf Post
-				<textarea name="" id="" rows="5"></textarea>
-				<button type="button">
-					Отправить
-				</button>
-			</div>
-			<ul>
-				<li>
-					<Post
-						imgId="https://i.pravatar.cc/150?img=1"
-						message="Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!"
-						likesCount={12}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId="https://i.pravatar.cc/300?img=2"
-						message="Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!"
-						likesCount={1}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId="https://i.pravatar.cc/300?img=3"
-						message="Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия."
-						likesCount={124}
-					/>
-				</li>
-			</ul>
+			<h2>My Posts</h2>
+			<textarea name="" id="" rows="5"></textarea>
+			<button type="button">
+				Отправить
+			</button>
 		</div>
-	);
+		<ul className={p.postsBlock__list}>
+			<li className={p.postsBlock__item}>
+				<Post
+					imgId="https://i.pravatar.cc/150?img=1"
+					message="Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!"
+					likesCount={12}
+				/>
+			</li>
+			<li className={p.postsBlock__item}>
+				<Post
+					imgId="https://i.pravatar.cc/300?img=2"
+					message="Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!"
+					likesCount={1}
+				/>
+			</li>
+			<li className={p.postsBlock__item}>
+				<Post
+					imgId="https://i.pravatar.cc/300?img=3"
+					message="Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия."
+					likesCount={124}
+				/>
+			</li>
+		</ul>
+	</div>
+);
 
 export default MyPosts;
