0e09222977d02cb829bdddadef20ae242e9d8d18
Семён Крекотун <samkrekotyn@gmail.com>
2023-04-14 15:25:38 +0300

    refactor

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,11 +5,15 @@ import p from './MyPosts.module.css';
 
 const MyPosts = () => {
 
-	let postData = [
+	const postsData = [
 		{id: 1, message: 'Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!', likesCount: 12, src: 'https://i.pravatar.cc/150?img=1', },
 		{id: 2, message: 'Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!', likesCount: 11111, src: 'https://i.pravatar.cc/300?img=2', },
 		{id: 3, message: 'Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия.', likesCount: 33, src: 'https://i.pravatar.cc/300?img=3', },
+		{id: 4, message: 'Привет, друзья! Я очень ... места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия.', likesCount: 3, src: 'https://i.pravatar.cc/300?img=4', },
 	]
+
+	const posts = postsData.map(post => <li><Post imgId={post.src} message={post.message} likesCount={post.likesCount} /></li>
+	)
 
 	return (
 		<div className={p.myPosts}>
@@ -21,27 +25,7 @@ const MyPosts = () => {
 				</button>
 			</div>
 			<ul>
-				<li>
-					<Post
-						imgId={postData[0].src}
-						message={postData[0].message}
-						likesCount={postData[0].likesCount}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId={postData[1].src}
-						message={postData[1].message}
-						likesCount={postData[1].likesCount}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId={postData[2].src}
-						message={postData[2].message}
-						likesCount={postData[2].likesCount}
-					/>
-				</li>
+				{posts}
 			</ul>
 		</div>
 	);
