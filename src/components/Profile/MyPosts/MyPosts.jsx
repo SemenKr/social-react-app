ab021978caa6363e3fd62be13b0d1cb6fd9cf8af
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-04-14 10:58:42 +0300

    MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,49 +1,41 @@
 import React from 'react';
 import Post from './Post/Post';
-import './MyPosts.module.css';
+import p from './MyPosts.module.css';
 
-const MyPosts = () => {
-
-	const postData = [
-		{id: 1, message: 'Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!', likesCount: 12, src: 'https://i.pravatar.cc/150?img=1', },
-		{id: 2, message: 'Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!', likesCount: 11111, src: 'https://i.pravatar.cc/300?img=2', },
-		{id: 3, message: 'Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия.', likesCount: 33, src: 'https://i.pravatar.cc/300?img=3', },
-	]
-
-	return (
+const MyPosts = () =>
+(
+	<div>
 		<div>
-			<div>
-				wtf Post
-				<textarea name="" id="" rows="5"></textarea>
-				<button type="button">
-					Отправить
-				</button>
-			</div>
-			<ul>
-				<li>
-					<Post
-						imgId={postData[0].src}
-						message={postData[0].message}
-						likesCount={postData[0].likesCount}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId={postData[1].src}
-						message={postData[1].message}
-						likesCount={postData[1].likesCount}
-					/>
-				</li>
-				<li>
-					<Post
-						imgId={postData[2].src}
-						message={postData[2].message}
-						likesCount={postData[2].likesCount}
-					/>
-				</li>
-			</ul>
+			wtf Post
+			<textarea name="" id="" rows="5"></textarea>
+			<button type="button">
+				Отправить
+			</button>
 		</div>
-	);
-};
+		<ul>
+			<li>
+				<Post
+					imgId="https://i.pravatar.cc/150?img=1"
+					message="Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!"
+					likesCount={12}
+				/>
+			</li>
+			<li>
+				<Post
+					imgId="https://i.pravatar.cc/300?img=2"
+					message="Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!"
+					likesCount={1}
+				/>
+			</li>
+			<li>
+				<Post
+					imgId="https://i.pravatar.cc/300?img=3"
+					message="Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия."
+					likesCount={124}
+				/>
+			</li>
+		</ul>
+	</div>
+);
 
 export default MyPosts;
