ac7e668e577b99f6b6b78196a310b3f4cd4c4ca1
Семён Крекотун <samkrekotyn@gmail.com>
2023-04-16 23:45:27 +0300

    fix button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import style from './Button.module.css';
+// import React from 'react';
+// import style from './Button.module.css';
 
-const Button = (props) => {
-	return (
-		<button onClick={this.handleClick} className={style.btn} > {props.title}{props.icon}</button >
-	)
-}
+// const Button = (props) => {
+// 	return (
+// 		<button onClick={this.handleClick} className={style.btn} > {props.title}{props.icon}</button >
+// 	)
+// }
 
-export default Button;
+// export default Button;
