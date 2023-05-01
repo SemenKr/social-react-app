4ae0a0b83873255d0c67983b786f174623ee8f93
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-01 16:45:46 +0300

    refactoring NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import nav from './NavBar.module.css';
 import NavMenuItem from './NavMenu/NavMenu';
 import Friend from './Friends/Friends';
+import StoreContext from "../Redux/storeContext";
 
 
 
-const NavBar = (props) => {
+const NavBar = () => {
 
 
-	const links = props.state.navMenu.links.map(link => <li className={nav.navigation__item}><NavMenuItem name={link.name} path={link.path} /></li>);
-	const friends = props.state.navMenu.users.map(friend => <li className={nav.friends__item}><Friend name={friend.name} src={friend.src} alt={friend.alt} /></li>)
+		return (
+		<StoreContext.Consumer>
+			{store => {
 
-	return (
-		<div className={nav.navBar}>
-			<nav className={`${nav.navBar__navigation} ${nav.navigation}`}>
-				<ul className={nav.navigation__list}>
-					{links}
-				</ul>
-			</nav>
-			<div className={`${nav.navBar__friends} ${nav.friends}`}>
-				<ul className={nav.friends__list}>
-					{friends}
-				</ul>
-			</div>
+				const state = store.getState().navBar;
+				const links = state.navMenu.links.map(link => <li className={nav.navigation__item}><NavMenuItem name={link.name} path={link.path} /></li>);
+				const friends = state.navMenu.users.map(friend => <li className={nav.friends__item}><Friend name={friend.name} src={friend.src} alt={friend.alt} /></li>)
 
-		</div>
+
+
+				return (
+					<div className={nav.navBar}>
+						<nav className={`${nav.navBar__navigation} ${nav.navigation}`}>
+							<ul className={nav.navigation__list}>
+								{links}
+							</ul>
+						</nav>
+						<div className={`${nav.navBar__friends} ${nav.friends}`}>
+							<ul className={nav.friends__list}>
+								{friends}
+							</ul>
+						</div>
+
+					</div>
+				)
+			}
+
+			}
+		</StoreContext.Consumer>
+
 
 	);
 };
