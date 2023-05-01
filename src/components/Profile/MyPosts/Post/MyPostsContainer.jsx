5b5919a7ef175f266636a8bbbcde44f2655c400b
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-01 16:47:41 +0300

    refactoring storeContext.js

--- src/components/Profile/MyPosts/Post/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/Post/MyPostsContainer.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import MyPosts from "../MyPosts";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../Redux/profile-reducer";
+import StoreContext from "../../../Redux/storeContext";
 
 
-const MyPostsContainer = (props) => {
-
-    let state = props.store.getState();
-
-
-    // берет функцию из state которая берет newPostText из state и добавляет state объект с новым постом
-    const addPost = () => {
-        props.store.dispatch(addPostActionCreator());
-    }
-
-    // следит за изменением в textarea и с помощью функции из state добавляет изменения в state
-    const onPostChange = (text) => {
-        let action = updateNewPostTextActionCreator(text);
-        props.store.dispatch(action);
-    }
+const MyPostsContainer = () => {
 
     return (
-        <MyPosts updateNewPostText={onPostChange}
-                 addPost={addPost}
-                 posts={state.profilePage}/>
+        <StoreContext.Consumer>
+            {store => {
+                const addPost = () => {
+                    store.dispatch(addPostActionCreator());
+                }
+
+                // следит за изменением в textarea и с помощью функции из state добавляет изменения в state
+                const onPostChange = (text) => {
+                    let action = updateNewPostTextActionCreator(text);
+                    store.dispatch(action);
+                }
+                return (
+
+                    <MyPosts updateNewPostText={onPostChange}
+                             addPost={addPost}
+                             posts={store.getState().profilePage}/>
+                )
+            }
+            }
+        </StoreContext.Consumer>
+
     );
 }
 
