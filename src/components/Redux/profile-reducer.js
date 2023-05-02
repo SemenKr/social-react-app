a95554e0daab09639fd38f360e724de582a5bad3
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-02 19:13:10 +0300

    refactoring Profile

--- src/components/Redux/profile-reducer.js
+++ src/components/Redux/profile-reducer.js
@@ -33,21 +33,31 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 12,
                 src: 'https://i.pravatar.cc/150?img=5',
             };
-            state.postsData.push(newPost);
-            state.newPostText = '';
-            return state
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText;
-            return state
+            return {
+                ...state,
+                postsData: [...state.postsData, newPost],
+                newPostText: '',
+
+            }
+        }
+
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText,
+            }
+
+        }
         default:
             return state;
+
     }
 
 
