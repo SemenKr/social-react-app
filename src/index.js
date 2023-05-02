5c9f5aaee21a24f046c0fba5c4fcd21291e1323d
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-02 12:56:31 +0300

    add Provider around App react-redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import store from './components/Redux/redux-store';
-import StoreContext from "./components/Redux/storeContext";
+import {Provider} from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const renderEntireTree = () => {
 	root.render(
 		<React.StrictMode>
-			<StoreContext.Provider value={store}>
+			<Provider store={store}>
 				<App />
-			</StoreContext.Provider>
+			</Provider>
 
 		</React.StrictMode>
 	);
