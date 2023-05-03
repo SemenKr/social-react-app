dce7472d906c6cecffcee00ff1edba32a678f80d
Семён Крекотун <samkrekotyn@gmail.com>
2023-05-03 21:05:27 +0300

    refactoring users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,12 +2,50 @@ import React from "react";
 import styles from './users.module.css';
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        props.setUsers([
+                {
+                    id: 1,
+                    followed: true,
+                    fullName: 'Antonina',
+                    status: 'smart fitness in my life',
+                    location: {city: 'SPB', country: 'russia'},
+                    src: 'https://i.pravatar.cc/150?img=1',
+                },
+                {
+                    id: 2,
+                    followed: true,
+                    fullName: 'Andrew',
+                    status: 'hard work',
+                    location: {city: 'SPB', country: 'russia'},
+                    src: 'https://i.pravatar.cc/150?img=2',
+                },
+                {
+                    id: 3,
+                    followed: false,
+                    fullName: 'Nadin',
+                    status: 'design in my life',
+                    location: {city: 'SPB', country: 'russia'},
+                    src: 'https://i.pravatar.cc/150?img=6',
+                },
+                {
+                    id: 4,
+                    followed: false,
+                    fullName: 'Alena',
+                    status: 'style',
+                    location: {city: 'SPB', country: 'russia'},
+                    src: 'https://i.pravatar.cc/150?img=11',
+                },
+            ]
+        )
+    }
+
 
     return <>
         <h3>Users</h3>
         <ul className={styles.users}>
             {
-                props.users.map(user => <li  className={styles.userCard} key={user.id}>
+                props.users.map(user => <li className={styles.userCard} key={user.id}>
                     <div className={styles.userCard__top}></div>
                     <div className={styles.userCard__bottom}>
                         <div className={styles.imageWrapper}><img className={styles.image} src={user.src} alt=""/></div>
@@ -21,9 +59,13 @@ const Users = (props) => {
                                 <p className={styles.p}>{user.location.country}</p>
                                 <p className={styles.p}>{user.location.city}</p>
                             </div>
-                            { user.followed
-                                ? <button onClick={() => {props.unfollow(user.id)}} className={styles.p}>Unfollow</button>
-                                : <button onClick={() => {props.follow(user.id)}} className={styles.p}>Follow</button> }
+                            {user.followed
+                                ? <button onClick={() => {
+                                    props.unfollow(user.id)
+                                }} className={styles.p}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(user.id)
+                                }} className={styles.p}>Follow</button>}
 
                         </div>
 
@@ -31,7 +73,7 @@ const Users = (props) => {
                 </li>)
             }
         </ul>
-  
+
     </>
 }
 
