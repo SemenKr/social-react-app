6b822b79d908716e50b4a2fd2c1a37b87d73e06e
Семён Крекотун <samkrekotyn@gmail.com>
2023-05-05 14:23:21 +0300

    добавляет стили в Users

--- src/components/Redux/users-reducer.js
+++ src/components/Redux/users-reducer.js
@@ -3,43 +3,43 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 let initialState = {
-     users: [],
+	users: [],
 }
 
 const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FOLLOW:
-            return {
-                // копируем state и пробегаемся по всем users
-                ...state,
-                users: state.users.map(user => {
-                    // если id user совпвдвет с userID из action то возвращаем копию обьекта user и добавляем followed: true
-                    if (user.id === action.userId) {
-                        return {...user, followed: true}
-                    }
-                    return user
-                })
-            }
+	switch (action.type) {
+		case FOLLOW:
+			return {
+				// копируем state и пробегаемся по всем users
+				...state,
+				users: state.users.map(user => {
+					// если id user совпадает с userID из action то возвращаем копию объекта user и добавляем followed: true
+					if (user.id === action.userId) {
+						return {...user, followed: true}
+					}
+					return user
+				})
+			}
 
-        case UNFOLLOW:
-            return {
-                ...state,// копируем state и пробегаемся по всем users
-                users: state.users.map(user => {
-                    // если id user совпвдвет с userID из action то возвращаем копию обьекта user и добавляем followed: false
-                    if (user.id === action.userId) {
-                        return {...user, followed: false}
-                    }
-                    return user
-                })
-            }
-        case SET_USERS: {
-            // для добавления users мы будем брать копию state и в users будем к копии users добавлять users из action
-            return { ...state, users: [...action.users] }
-        }
-        default:
-            return state;
+		case UNFOLLOW:
+			return {
+				...state,// копируем state и пробегаемся по всем users
+				users: state.users.map(user => {
+					// если id user совпадает с userID из action то возвращаем копию объекта user и добавляем followed: false
+					if (user.id === action.userId) {
+						return {...user, followed: false}
+					}
+					return user
+				})
+			}
+		case SET_USERS: {
+			// для добавления users мы будем брать копию state и в users будем к копии users добавлять users из action
+			return {...state, users: [...action.users]}
+		}
+		default:
+			return state;
 
-    }
+	}
 
 
 }
