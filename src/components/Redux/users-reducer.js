1a1ed6af2270a7e88f75fe2cf9a173bed8d6b4c5
Семён Крекотун <samkrekotyn@gmail.com>
2023-05-12 10:24:59 +0300

    добавляет currentPage и total users count

--- src/components/Redux/users-reducer.js
+++ src/components/Redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
 
 let initialState = {
 	users: [],
+	pageSize: 5,
+	totalUsersCount: 0,
+	currentPage: 1,
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -36,6 +41,12 @@ const usersReducer = (state = initialState, action) => {
 			// для добавления users мы будем брать копию state и в users будем к копии users добавлять users из action
 			return {...state, users: action.users}
 		}
+		case SET_CURRENT_PAGE: {
+			return {...state, currentPage: action.currentPage}
+		}
+		case SET_TOTAL_USERS_COUNT: {
+			return {...state, totalUsersCount: action.count}
+		}
 		default:
 			return state;
 
@@ -47,5 +58,7 @@ const usersReducer = (state = initialState, action) => {
 export const followActionCreator = userId => ({type: FOLLOW, userId});
 export const unfollowActionCreator = userId => ({type: UNFOLLOW, userId});
 export const setUsersActionCreator = users => ({type: SET_USERS, users});
+export const setCurrentPageActionCreator = currentPage => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountActionCreator = totalUsersCount => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
 
 export default usersReducer;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -8,17 +8,49 @@ import userPhoto from '../../assets/images/user.png'
 class Users extends Component {
 
 	componentDidMount() {
-		axios.get("https://social-network.samuraijs.com/api/1.0/users")
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize} `)
 			.then(response => {
 				this.props.setUsers(response.data.items);
+				this.props.setTotalUsersCount(response.data.totalCount);
+
+			});
+	}
+
+	onPageChange = (pageNumber) => {
+		this.props.setCurrentPage(pageNumber)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize} `)
+			.then(response => {
+				this.props.setUsers(response.data.items);
+
 			});
 	}
 
 	render() {
 
+		let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+		let pages = [];
+		for (let i = 1; i <= pagesCount; i++) {
+
+			pages.push(i);
+
+		}
+
+		let currentPage = this.props.currentPage;
+		let currentPageFirst = ((currentPage - 5) < 0) ? 0 : currentPage - 5;
+		let currentPageLast = currentPage + 5;
+		let slicedPages = pages.slice(currentPageFirst, currentPageLast);
+
+
 
 		return <>
 			<h3>Users</h3>
+			<ol className={styles.pagination__list}>
+				{slicedPages.map((page, index) => {
+					return <li key={index}><button onClick={() => {this.onPageChange(page)}} className={`${styles.pagination__item} ${this.props.currentPage === page && styles._active}`} >{page}</button></li>
+				})}
+
+			</ol >
 			<ul className={styles.users}>
 				{this.props.users.map(user => <li className={styles.userCard} key={user.id}>
 					<div className={styles.userCard__top}></div>
