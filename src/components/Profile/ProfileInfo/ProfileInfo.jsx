6fc08dc145c1a64ae71e7aab2efe06bc3da16329
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-14 23:29:40 +0300

    добавляет стили профайлИнфо и пропсы

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import p from './ProfileInfo.module.css';
+import p from './ProfileInfo.module.scss';
+import Preloader from "../../common/Preloader/Preloader";
 
 const ProfileInfo = (props) => {
+
+	if (!props.profile) {
+		return <Preloader />
+	}
 	return (
 		<div className={p.info}>
-			<div className={p.info__imageWrapper}>
-				<img className={p.info__image} src="./img/photo-sam.jpg" alt="yo" />
+			<div className={p.info__top}>
+				<img src={props.profile.photos.large} alt="" />
+			</div>
+			<div className={p.info__content}>
+				<div className={p.info__imageWrapper}>
+					<img className={p.info__image} src={props.profile.photos.small} alt="yo" />
+				</div>
+
+				<p className={p.info__description}>{props.profile.aboutMe}</p>
 			</div>
 
-			<p className={p.info__description}>{props.description}</p>
+
 		</div>
 	)
 }
