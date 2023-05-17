fad192a7274bb1b6db13a5b3a68d0cae9a39245e
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-17 06:32:07 +0300

    добавляет в  reducer combain auth-reducer

--- src/components/Redux/redux-store.js
+++ src/components/Redux/redux-store.js
@@ -3,14 +3,16 @@ import navBarReducer from "./navBar-reducer";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from './auth-reduce';
 
 let reducers = combineReducers({
-    navBar: navBarReducer,
-    profilePage: profileReducer,
-    dialogPage: dialogsReducer,
-    usersPage: usersReducer,
+	navBar: navBarReducer,
+	profilePage: profileReducer,
+	dialogPage: dialogsReducer,
+	usersPage: usersReducer,
+	auth: authReducer,
 })
 let store = createStore(reducers);
 
 window.store = store;
-export default store;
+export default store; 
