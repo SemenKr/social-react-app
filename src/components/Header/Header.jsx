e3e0df2e6d84b1c70ab336ded7c51878681966a0
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-05-17 06:35:35 +0300

    добавляет header Container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,22 @@
-/** @format */
-
 import React from 'react';
-import h from './Header.module.css';
+import h from './Header.module.scss';
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
-   return (
-      <header className={h.header}>
-         <img
-            className={h.header__image}
-            src="https://img.logoipsum.com/244.png"
-            width={'150'}
-            height={'40'}
-            alt="logo"
-         />
-      </header>
-   );
+const Header = (props) => {
+	return (
+		<header className={h.header}>
+			<img className={h.header__image}
+				src="https://img.logoipsum.com/244.png"
+				width={'150'}
+				height={'40'}
+				alt="logo" />
+
+			<div className={`${h.header__login} ${h.login}`}>
+				{props.isAuth ? <NavLink to={'/login'}>{props.login}</NavLink > : <NavLink to={'/login'}>Login</NavLink >}
+
+			</div>
+		</header>
+	);
 };
 
 export default Header;
