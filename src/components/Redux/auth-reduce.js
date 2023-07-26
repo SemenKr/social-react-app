86fbad51d4c120701635c8e565be5cd3b896c7a4
semenkr <samkrekotyn@gmail.com>
2023-07-26 14:01:58 +0300

    Refactor authReducer to use async/await
    This commit refactors the authReducer component by replacing Promise-based syntax with the more modern async/await syntax. This makes the code more readable and easier to understand. Additionally, catch blocks have been added for better error handling. The initialState has also been changed from a let to a const variable for improved security and predictability.

--- src/components/Redux/auth-reduce.js
+++ src/components/Redux/auth-reduce.js
@@ -2,69 +2,74 @@ import {authAPI} from "../../api/api";
 
 const SET_USER_DATA = 'SET-USER-DATA';
 
-let initialState = {
-	id: null,
-	login: null,
-	email: null,
-	isAuth: false,
+const initialState = {
+    id: null,
+    login: null,
+    email: null,
+    isAuth: false,
 }
 
 const authReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case SET_USER_DATA:
-			return {
-				...state,
-				...action.payload,// склеиваем 2 объекта state и data из action.data
-			}
-		default:
-			return state;
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.payload,// склеиваем 2 объекта state и data из action.data
+            }
+        default:
+            return state;
 
-	}
+    }
 
 
 }
 
-export const setAuthUserData = (id, login, email, isAuth) => ({type: SET_USER_DATA, payload: {id: id, login: login, email: email, isAuth}});
+export const setAuthUserData = (id, login, email, isAuth) => ({
+    type: SET_USER_DATA,
+    payload: {id: id, login: login, email: email, isAuth}
+});
 
 export const getAuthUserData = () => {
-	return (dispatch) => {
-		authAPI.getAuthMe()
-			.then(data => {
-				if (data.resultCode === 0) {
-					let {id, login, email} = data.data;
-					dispatch(setAuthUserData(id, login, email, true));
-				}
-			});
-	}
-
+    return async (dispatch) => {
+        try {
+            const data = await authAPI.getAuthMe();
+            if (data.resultCode === 0) {
+                let {id, login, email} = data.data;
+                dispatch(setAuthUserData(id, login, email, true));
+            }
+        } catch (error) {
+			console.error(error);
+        }
+    }
 }
 
 export const login = (email, password, rememberMe) => {
-	return async (dispatch) => {
-		try {
-			const response = await authAPI.login(email, password, rememberMe);
-			if (response.data.resultCode === 0) {
-				dispatch(getAuthUserData());
-			} else {
-				throw new Error("Ошибка входа. Проверьте введенные данные.");
-			}
-		} catch (error) {
-			throw error;
-		}
-	};
+    return async (dispatch) => {
+        try {
+            const response = await authAPI.login(email, password, rememberMe);
+            if (response.data.resultCode === 0) {
+                dispatch(getAuthUserData());
+            } else {
+                throw new Error("Ошибка входа. Проверьте введенные данные.");
+            }
+        } catch (error) {
+			console.error(error);
+        }
+    };
 };
 
 
-
 export const logout = () => {
-	return (dispatch) => {
-		authAPI.logout()
-			.then(response => {
-				if (response.data.resultCode === 0) {
-					dispatch(setAuthUserData(null, null, null, false));
-				}
-			});
-	};
+    return async (dispatch) => {
+		try {
+			const response = await authAPI.logout();
+			if (response.data.resultCode === 0) {
+				dispatch(setAuthUserData(null, null, null, false));
+			}
+		} catch (error) {
+			console.error(error);
+		}
+    };
 }
 
 export default authReducer;
