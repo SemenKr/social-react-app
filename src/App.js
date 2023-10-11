6c67de34ff4135beb74282148972cb72e3f371c9
semenkr <samkrekotyn@gmail.com>
2023-10-11 19:24:03 +0300

    Refactored class component to functional with useEffect in App.js
    Refactored the App class component to a functional component and utilized the useEffect hook for initialization. This optimizes rendering and enhances the readability of the code by simplifying the component structure.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.scss';
-import {Routes, Route, useParams} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
@@ -9,63 +9,47 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from "./components/Login/Login";
-import {initializeApp} from "./components/Redux/app-reduce";
-import {connect} from "react-redux";
+import { initializeApp } from "./components/Redux/app-reduce";
+import { connect } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
-import {compose} from "redux";
+import { compose } from "redux";
 
+function App({ initialized, initializeApp }) {
+    // Используем хук useEffect для выполнения инициализации при монтировании компонента.
+    useEffect(() => {
+        initializeApp();
+    }, [initializeApp]); // Зависимость от initializeApp, чтобы запустить инициализацию.
 
-class App extends React.Component {
-    // Этот метод вызывается, когда компонент монтируется в DOM.
-    componentDidMount() {
-        this.props.initializeApp();// Инициализируем приложение.
+    // Если приложение ещё не инициализировано, отображаем индикатор загрузки.
+    if (!initialized) {
+        return <Preloader />;
     }
 
-    render() {
-        // Если приложение ещё не инициализировано, отображаем индикатор загрузки.
-        if (!this.props.initialized) {
-            return <Preloader/>
-        }
-        // Если приложение инициализировано, отображаем основное содержимое.
-        return (
-            <div className="app-wrapper">
-                <HeaderContainer/>
-                <NavBarContainer/>
-                <div className="app__content">
-                    <Routes>
-                        <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
-                        <Route path="/messages/*" element={<DialogsContainer/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/users' element={<UsersContainer/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/login' element={<Login/>}/>
-                    </Routes>
-                </div>
+    // Если приложение инициализировано, отображаем основное содержимое.
+    return (
+        <div className="app-wrapper">
+            <HeaderContainer />
+            <NavBarContainer />
+            <div className="app__content">
+                <Routes>
+                    <Route path='/profile/:userId?' element={<ProfileContainer />} />
+                    <Route path="/messages/*" element={<DialogsContainer />} />
+                    <Route path='/news' element={<News />} />
+                    <Route path='/users' element={<UsersContainer />} />
+                    <Route path='/music' element={<Music />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
             </div>
-
-        );
-    }
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
     initialized: state.app.initialized // Сопоставляем свойство initialized из Redux состояния с props.
-})
+});
 
-// Функция withRouter позволяет передавать связанные с маршрутизатором свойства.
-function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-        // let location = useLocation();
-        // let navigate = useNavigate();
-        let params = useParams();// Получаем параметры из URL.
-        return (
-            <Component {...props} router={{params}}/>// Передаем параметры как свойство router.
-        );
-    }
+// Компонент App остается тем же, только теперь это функциональный компонент.
 
-    return ComponentWithRouterProp;
-}
-
-// С помощью compose объединяем withRouter и connect, чтобы применить их к компоненту App.
 export default compose(
-    withRouter,
-    connect(mapStateToProps, {initializeApp}))(App)
+    connect(mapStateToProps, { initializeApp })
+)(App);
