ae88ab7779791c62c13912e1f1737111442b6261
semenkr <samkrekotyn@gmail.com>
2023-10-12 07:20:47 +0300

    Refactor profile reducer for improved error handling
    The profile reducer in profile-reducer.js has been significantly refactored. New action creators and thunks were implemented to fetch, update and save user profile data, status, and photos through API calls. Error handling for these asynchronous actions was also improved with try-catch blocks to log error messages in case of failures. The switch-case operations in the reducer function have also been simplified. This restructuring results in cleaner and more manageable code, making it easier to trace the flow of data and handle errors.

--- src/components/Redux/profile-reducer.js
+++ src/components/Redux/profile-reducer.js
@@ -1,48 +1,88 @@
-import {profileAPI} from "../../api/api";
+import { profileAPI } from "../../api/api";
 
-
+// Определение типов действий (action types)
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
 const SET_STATUS = 'SET-STATUS';
 const SET_PHOTO_SUCCESS = 'SET_PHOTO_SUCCESS';
 const SET_PROFILE_DATA_SUCCESS = 'SET-PROFILE-DATA-SUCCESS';
 
+// Action creators
+export const addPostActionCreator = (text) => ({ type: ADD_POST, text });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
+export const setStatus = (status) => ({ type: SET_STATUS, status });
+export const savePhotoSuccess = (photos) => ({ type: SET_PHOTO_SUCCESS, photos });
+export const setProfileData = (profileData) => ({ type: SET_PROFILE_DATA_SUCCESS, profileData });
+
+// Thunks
+export const getProfileUserThunk = (userId) => async (dispatch) => {
+	try {
+		const data = await profileAPI.getProfileUser(userId);
+		dispatch(setUserProfile(data));
+	} catch (error) {
+		console.error('Failed to get user profile:', error);
+	}
+};
+
+export const getStatus = (userId) => async (dispatch) => {
+	const response = await profileAPI.getStatus(userId);
+	dispatch(setStatus(response.data));
+};
+
+export const updateStatus = (status) => async (dispatch) => {
+	try {
+		const response = await profileAPI.updateStatus(status);
+		if (response.data.resultCode === 0) {
+			dispatch(setStatus(status));
+		} else {
+			// Если resultCode не равен 0, обрабатываем ошибку
+			console.error('Failed to update status:', response.data.messages);
+		}
+	} catch (error) {
+		console.error('Error while updating status:', error.message);
+
+		if (error.response) {
+			// Если есть ответ от сервера, проверяем статус ошибки HTTP
+			console.error('HTTP Status:', error.response.status);
+			console.error('HTTP Status Text:', error.response.statusText);
+		} else {
+			// В случае, если нет ответа от сервера
+			console.error('No response from server');
+		}
+	}
+};
+
+
+
+
+export const savePhoto = (photoFile) => async (dispatch) => {
+	const response = await profileAPI.savePhoto(photoFile);
+	if (response.resultCode === 0) {
+		dispatch(savePhotoSuccess(response.data.photos));
+	}
+};
+
+export const saveProfileData = (profileData) => async (dispatch, getState) => {
+	const userId = getState().auth.id;
+	const response = await profileAPI.saveProfileData(profileData);
+	if (response.data.resultCode === 0) {
+		dispatch(getProfileUserThunk(userId));
+	}
+};
+
+// Начальное состояние (initial state) для редюсера
 const initialState = {
-	postsData: [
-		{
-			id: 1,
-			message: 'Привет, мои дорогие подписчики! Я так рада, что вы зашли на мой блог. Здесь я буду писать о моей жизни, о моих путешествиях и о том, что меня вдохновляет. Буду рада вашим комментариям и предложениям!',
-			likesCount: 12,
-			src: 'https://i.pravatar.cc/150?img=1',
-		},
-		{
-			id: 2,
-			message: 'Приветствую всех, кто зашел на мой блог! Я специализируюсь на теме здоровья и фитнеса, и буду рад делиться с вами своим опытом и знаниями. Надеюсь, что мои статьи помогут вам быть здоровыми и счастливыми!',
-			likesCount: 11111,
-			src: 'https://i.pravatar.cc/300?img=2',
-		},
-		{
-			id: 3,
-			message: 'Привет, друзья! Я очень люблю путешествовать и открывать новые места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия.',
-			likesCount: 33,
-			src: 'https://i.pravatar.cc/300?img=3',
-		},
-		{
-			id: 4,
-			message: 'Привет, друзья! Я очень ... места. В моем блоге я буду рассказывать о своих приключениях и делиться с вами полезными советами о том, как лучше всего планировать свои путешествия.',
-			likesCount: 3,
-			src: 'https://i.pravatar.cc/300?img=4',
-		},
-	],
+	postsData: [],
 	newPostText: '',
 	profile: null,
 	status: 'double click here to change status',
-}
+};
 
+// Редюсер принимает текущее состояние (state) и действие (action), возвращает новое состояние.
 const profileReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case ADD_POST: {
-			let newPost = {
+		case ADD_POST:
+			const newPost = {
 				id: Date.now(),
 				message: action.text,
 				likesCount: 12,
@@ -52,90 +92,24 @@ const profileReducer = (state = initialState, action) => {
 				...state,
 				postsData: [...state.postsData, newPost],
 				newPostText: '',
-
-			}
-		}
-		case SET_USER_PROFILE: {
-			return {...state, profile: action.profile}
-		}
-		case SET_STATUS: {
-			return {...state, status: action.status}
-		}
-		case SET_PHOTO_SUCCESS: {
+			};
+		case SET_USER_PROFILE:
+			return { ...state, profile: action.profile };
+		case SET_STATUS:
+			return { ...state, status: action.status };
+		case SET_PHOTO_SUCCESS:
 			return {
 				...state,
-				profile: {...state.profile, photos: action.photos}
+				profile: { ...state.profile, photos: action.photos },
 			};
-		}
-		case SET_PROFILE_DATA_SUCCESS: {
+		case SET_PROFILE_DATA_SUCCESS:
 			return {
 				...state,
-				profile: {...state.profile, profile: action.profile}
+				profile: { ...state.profile, profile: action.profile },
 			};
-		}
 		default:
 			return state;
-
 	}
-
-
-}
-
-export const addPostActionCreator = (text) => ({type: ADD_POST, text});
-
-export const setUserProfile = profile => ({type: SET_USER_PROFILE, profile});
-export const setStatus = (status) => ({type: SET_STATUS, status})
-const savePhotoSuccess = (photos) => ({
-	type: SET_PHOTO_SUCCESS,
-	photos // Предполагая, что сервер возвращает объект с ключом "photos"
-});
-
-export const setProfileData = (profileData) => ({type: SET_PROFILE_DATA_SUCCESS, profileData})
-
-
-export const getProfileUserThunk = (userId) => {
-	return async dispatch => {
-		try	{
-			const data = await profileAPI.getProfileUser(userId);
-			dispatch(setUserProfile(data));
-		} catch (error) {
-			console.error('Failed to get user profile:', error);
-		}
-	}
-}
-
-export const getStatus = userId => (dispatch) => {
-	profileAPI.getStatus(userId)
-		.then(response => {
-			dispatch(setStatus(response.data))
-		})
-}
-
-export const updateStatus = status => async (dispatch) => {
-	const response = await profileAPI.updateStatus(status);
-	if (response.data.resultCode === 0) {
-		dispatch(setStatus(status))
-	}
-}
-
-export const savePhoto = (photoFile) => async (dispatch) => {
-	const response = await profileAPI.savePhoto(photoFile);
-	if (response.resultCode === 0) {
-		dispatch(savePhotoSuccess(response.data.photos))
-
-	}
-	console.log(response.data.photos);
-}
-
-export const saveProfileData = (profileData) => async (dispatch, getState ) => {
-	const userId = getState().auth.id;
-	const response = await profileAPI.saveProfileData(profileData);
-	console.log(' Reducer profile',  profileData);
-
-	if (response.data.resultCode === 0) {
-		 dispatch(getProfileUserThunk(userId))
-
-	}
-}
+};
 
 export default profileReducer;
