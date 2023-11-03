a95de65b5040d1ad9fc26e1ced8593c1ada4c92e
semenkr <samkrekotyn@gmail.com>
2023-11-03 18:20:37 +0300

    Рефактор API вызывает и определяет типы
    Вызовы API были организованы в отдельные файлы (userAPI, authAPI, profileAPI) для лучшей организации и читаемости. Кроме того, некоторые определения типов были перенесены в эти файлы, обеспечивая более последовательную структуру. Также исправлена опечатка в псевдониме @PropertiesTypes в redux-store.ts.

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -1,8 +1,25 @@
 import {instance} from "./api.ts";
 
+interface User {
+    name: string;
+    id: number;
+    photos: {
+        small: string | null;
+        large: string | null;
+    };
+    status: string | null;
+    followed: boolean;
+}
+
+interface UsersData {
+    items: User[];
+    totalCount: number;
+    error: string | null;
+}
+
 export const userAPI = {
     getUsers(currentPage: number = 1, pageSize: number = 6) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize} `)
+        return instance.get<UsersData>(`users?page=${currentPage}&count=${pageSize} `)
             .then(response => response.data);
     },
     deleteFollow(userId: number) {
