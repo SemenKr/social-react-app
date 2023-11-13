a029671f09a9189a713deb173fba6b02926ebf74
semenkr <samkrekotyn@gmail.com>
2023-11-13 19:35:11 +0300

    Update DialogsContainer with Authentication check
    The DialogsContainer component has been updated significantly - the code has been refactored with organized import sections and comments in the native language. The component now uses the "connect" function from the "react-redux" library to connect to the Redux store and is wrapped with the "withAuthRedirect" HOC, improving user authentication by redirecting if the user is not authenticated. Lastly, the actions object from "dialogs-reducer.ts" is spread when passed to connect to make them accessible as props in the Dialogs component.

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,24 +1,51 @@
- import {actions} from '../Redux/dialogs-reducer';
+// Импортируем действия из dialogs-reducer.ts
+import {actions} from '../Redux/dialogs-reducer.ts';
+
+// Импортируем компонент Dialogs
 import Dialogs from "./Dialogs";
+
+// Импортируем функцию connect из react-redux для подключения компонента к Redux store
 import {connect} from "react-redux";
+
+// Импортируем HOC для редиректа при отсутствии авторизации
 import {withAuthRedirect} from "../hoc/withAuthRedirect";
- import {compose} from "redux";
- import {AppStateType} from "../Redux/redux-store";
- import {ComponentType} from "react";
 
+// Импортируем функцию compose из redux для последовательного применения функций усилителей
+import {compose} from "redux";
 
+// Импортируем тип AppStateType из redux-store
+import {AppStateType} from "../Redux/redux-store";
+
+// Импортируем тип ComponentType из react
+import {ComponentType} from "react";
+
+// Функция mapStateToProps выбирает те части данных из store, которые нужны компоненту Dialogs
 let mapStateToProps = (state: AppStateType) => {
 	return {
 		dialogs: state.dialogPage,
 	}
 };
 
-// создаем HOC compose в который берет Dialogs, обрабатывает с помощью withAuthRedirect, потом результат передает в connect
-export default compose<ComponentType>(
-	connect(mapStateToProps, {
-		sendMessage: actions.sendMessage
-	}),
+// Создаем контейнерную компоненту DialogsContainer, которая подключена к Redux store и обернута в HOC withAuthRedirect
+const DialogsContainer = compose<ComponentType>(
+	// Подключаем компонент Dialogs к Redux store
+	connect(mapStateToProps, {...actions}),
+	// Оборачиваем компонент Dialogs в HOC withAuthRedirect
 	withAuthRedirect
 )(Dialogs)
 
-//ghjthrf
+// Экспортируем DialogsContainer как модуль по умолчанию
+export default DialogsContainer
+
+
+ //Это контейнерная компонента для Dialogs в React-Redux приложении. Она использует функцию connect из библиотеки react-redux для подключения компонента к Redux store.
+//
+// mapStateToProps используется для выбора тех частей данных из store, которые компоненту Dialogs нужны для отображения.
+//
+// actions - это объект, содержащий action creators, которые были импортированы из dialogs-reducer.ts. Они передаются в connect вместе с mapStateToProps, чтобы они были доступны в качестве пропсов в компоненте Dialogs.
+//
+// withAuthRedirect - это высокоуровневая компонента (HOC), которая оборачивает Dialogs и редиректит пользователя, если он не авторизован.
+//
+// compose из Redux используется для применения нескольких функций усилителей (в данном случае, connect и withAuthRedirect) последовательно справа налево.
+//
+// В итоге, DialogsContainer экспортируется как компонент, который подключен к Redux store и обернут в withAuthRedirect.
