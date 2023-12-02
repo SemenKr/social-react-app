6f3248a078822b73637d91908489d13ff99ea707
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2023-12-02 23:54:36 +0300

    Upgrade Typescript and refactor codebase
    Upgraded the Typescript version to 5.3.2 to utilize advanced features and improve code quality. Refactored codebase by adding "typings" to "src", removing "es2015" from lib array in tsconfig.json, enhancing error handling in App.tsx, and adjusting style imports in declaration.d.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import {useEffect} from "react";
+/// <reference lib="dom" />
+
+import { useEffect } from "react";
 import './App.scss';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavBarContainer from "./components/NavBar/NavBarContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from './components/Header/HeaderContainer';
-import {initializeApp} from "./components/Redux/app-reduce";
-import {connect} from "react-redux";
+import { initializeApp } from "./components/Redux/app-reduce";
+import { connect } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
-import {compose} from "redux";
+import { compose } from "redux";
 import NotFoundPage from "./components/common/404error/Error404";
-import {Navigate} from "react-router";
-import {AppStateType} from "./components/Redux/redux-store";
-import {UserPage} from "./components/Users/UsersContainer";
-import {LoginPage} from "./components/Login/Login";
+import { AppStateType } from "./components/Redux/redux-store";
+import { UserPage } from "./components/Users/UsersContainer";
+import { LoginPage } from "./components/Login/Login";
 
-//const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
-//const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
-//
-// type MapPropsType = ReturnType<typeof mapStateToProps>
-//
-// type DispatchPropsType = { initializeApp: () => void }
+interface PromiseRejectionEvent extends Event {
+	reason?: any;
+}
 
-function App({initialized, initializeApp}: { initialized: boolean; initializeApp: () => void }) {
+function App({
+				 initialized,
+				 initializeApp
+			 }: {
+	initialized: boolean;
+	initializeApp: () => void;
+}) {
 	useEffect(() => {
 		// Добавляем слушатель события unhandledrejection
 		window.addEventListener("unhandledrejection", handlePromiseRejection);
 
 		// Функция-обработчик необработанных промисов
 		function handlePromiseRejection(event: PromiseRejectionEvent) {
-			const {reason} = event;
+			const { reason } = event;
 			console.error("Unhandled Promise Rejection:", reason);
-
-			// Опционально: предотвращение появления сообщения об ошибке в консоли браузера
-			event.preventDefault();
 		}
 
 		// Вызываем инициализацию при монтировании компонента
@@ -50,7 +51,6 @@ function App({initialized, initializeApp}: { initialized: boolean; initializeApp
 	if (!initialized) {
 		return <Preloader />;
 	}
-
 
 	// Если приложение инициализировано, отображаем основное содержимое.
 	return (
@@ -75,11 +75,9 @@ function App({initialized, initializeApp}: { initialized: boolean; initializeApp
 }
 
 const mapStateToProps = (state: AppStateType) => ({
-	initialized: state.app.initialized // Сопоставляем свойство initialized из Redux состояния с props.
+	initialized: state.app.initialized,
 });
 
-// Компонент App остается тем же, только теперь это функциональный компонент.
-
 export default compose(
-	connect(mapStateToProps, {initializeApp})
+	connect(mapStateToProps, { initializeApp })
 )(App);
